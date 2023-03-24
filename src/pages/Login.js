55002846fa2d413784ad08e2b0d36bcc3f066664
[DSamuel-Anumudu <samuelmaize55@yahoo.com>
2023-03-24 16:59:53 +0200

    signup page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
@@ -6,6 +7,12 @@ const Login = () => {
     password: "",
   });
   const { email, password } = formData;
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
   useEffect(() => {
     const labels = document.querySelectorAll(".form-control label");
@@ -19,10 +26,6 @@ const Login = () => {
         .join("");
     });
   }, []);
-
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
 
   return (
     <section>
@@ -38,7 +41,8 @@ const Login = () => {
               type="text"
               name="email"
               id="email"
-              // value={email}
+              value={email}
+              onChange={onChange}
               required
             />
             <label className="absolute top-[15px] left-0">Email</label>
@@ -47,20 +51,29 @@ const Login = () => {
             <input
               className="input py-4 px-0 block w-full max-w-xs bg-transparent rounded-none border-t-0 border-x-0 border-b-2 border-green-500 focus:border-b-[#add8e6] focus:outline-none valid:border-b-[#add8e6] valid:outline-none cursor-pointer"
               type={showPassword ? "text" : "password"}
-              // value={password}
+              value={password}
               name="password"
               id="password"
+              onChange={onChange}
               required
             />
             <label className="absolute top-[15px] left-0">Password</label>
           </div>
 
           <div className="password-checkbox">
-            <input type="checkbox" onClick={toggleShowPassword} />
+            <input
+              type="checkbox"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
             <span>Show Password</span>
           </div>
           <button className="btn normal-case">Login to your account</button>
         </form>
+        <div className="sign-up-link">
+          <p>
+            Don’t have an account? <Link to="/signup">Sign Up</Link>
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,99 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+  const { email, password, repeatPassword } = formData;
+
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  useEffect(() => {
+    const labels = document.querySelectorAll(".form-control label");
+    labels.forEach((label) => {
+      label.innerHTML = label.innerText
+        .split("")
+        .map(
+          (letter, idx) =>
+            `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`
+        )
+        .join("");
+    });
+  }, []);
+
+  return (
+    <section>
+      <div className="login-logo">
+        <h1>Logo</h1>
+      </div>
+      <div className="login-box">
+        <h1>Signup</h1>
+        <form onSubmit={}>
+          <div className="form-control relative w-72 mt-5 mx-0 mb-10">
+            <input
+              className="input py-4 px-0 block w-full max-w-xs bg-transparent rounded-none border-t-0 border-x-0 border-b-2 border-green-500 focus:border-b-[#add8e6] focus:outline-none valid:border-b-[#add8e6] valid:outline-none cursor-pointer"
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              required
+            />
+            <label className="absolute top-[15px] left-0">Email</label>
+          </div>
+          <div className="form-control relative w-72 mt-5 mx-0 mb-10">
+            <input
+              className="input py-4 px-0 block w-full max-w-xs bg-transparent rounded-none border-t-0 border-x-0 border-b-2 border-green-500 focus:border-b-[#add8e6] focus:outline-none valid:border-b-[#add8e6] valid:outline-none cursor-pointer"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              name="password"
+              id="password"
+              onChange={onChange}
+              required
+            />
+            <label className="absolute top-[15px] left-0">Password</label>
+          </div>
+          <div className="form-control relative w-72 mt-5 mx-0 mb-10">
+            <input
+              className="input py-4 px-0 block w-full max-w-xs bg-transparent rounded-none border-t-0 border-x-0 border-b-2 border-green-500 focus:border-b-[#add8e6] focus:outline-none valid:border-b-[#add8e6] valid:outline-none cursor-pointer"
+              type={showPassword ? "text" : "password"}
+              value={repeatPassword}
+              name="repeatPassword"
+              id="repeatPassword"
+              onChange={onChange}
+              required
+            />
+            <label className="absolute top-[15px] left-0 repeat-password">
+              Repeat Password
+            </label>
+          </div>
+          <div className="password-checkbox">
+            <input
+              type="checkbox"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+            <span>Show Password</span>
+          </div>
+          <button className="btn normal-case">Create an account</button>
+        </form>
+        <div className="sign-up-link">
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Signup;
